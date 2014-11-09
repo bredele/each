0f115a13f94be02391f11e6cf54cd2807e654182
bredele <olivier.wietrich@gmail.com>
2014-11-09 15:52:02 -0700

    refactor tests

--- test/each.js
+++ test/each.js
@@ -1,12 +1,16 @@
+
+/**
+ * Test dependencies.
+ */
+
 var each = require('..');
 var assert = require('assert');
 
-function compare(obj1, obj2){
-  return JSON.stringify(obj1) === JSON.stringify(obj2);
-}
 
-describe('Iteration Object', function(){
-  it('should iterate through each key/value pair', function(){
+
+describe('object', function() {
+
+  it('should iterate through each key/value pair', function() {
     var obj = {
       name : 'olivier',
       github : 'bredele'
@@ -15,24 +19,26 @@ describe('Iteration Object', function(){
     each(obj, function(key, val){
       results[key] = val;
     });
-    assert(compare(obj, results));
+    assert.deepEqual(obj, results);
   });
 });
 
-describe('Iteration Array', function(){
+describe('array', function() {
+
   it('should iterate through each values', function(){
     var obj = ['olivier', 'bredele'];
     var results = [];
-    //keys are array index
     each(obj, function(key, val){
       results[key] = val;
     });
-    assert(compare(obj, results));
+    assert.deepEqual(obj, results);
   });
 });
 
 
-describe('Iteration scope', function(){
+// note: should we keep the scope?
+describe('Iteration scope', function() {
+
   it('should apply callback in passed scope', function(){
     var obj = ['olivier', 'wietrich'];
 
@@ -44,10 +50,7 @@ describe('Iteration scope', function(){
       this.names.push(val);
     }, scope);
 
-    assert(compare(scope.names, obj));
+    assert.deepEqual(scope.names, obj);
   });
 });
 
-describe('Iteration Array-like', function(){
-
-});
